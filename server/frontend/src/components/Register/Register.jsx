c2baaa806887d9a2b4999fe3649a12f35e3cdf3d
Deolyn East <deolyneast@gmail.com>
2026-03-23 09:19:33 +0200

    Create Register.jsx

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,10 @@
+<div class="register_container">
+    <form onSubmit={register}>
+        <input type="text" name="userName" placeholder="Username" required />
+        <input type="text" name="first_name" placeholder="First Name" required />
+        <input type="text" name="last_name" placeholder="Last Name" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+</div>
