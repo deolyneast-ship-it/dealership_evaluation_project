050695c4a20e694cf54c91ee2d23dc827caf47d0
Deolyn East <deolyneast@gmail.com>
2026-03-23 11:19:35 +0200

    Update wsgi.py

--- djangoproj/wsgi.py
+++ djangoproj/wsgi.py
@@ -4,4 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproj.settings')
 
 application = get_wsgi_application()
-app = application # Vercel needs this 'app' variable
+app = application  # <--- THIS LINE IS CRITICAL FOR VERCEL
