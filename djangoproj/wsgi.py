a24721e668d23cade90c17468b1687bd3619e5ce
Deolyn East <deolyneast@gmail.com>
2026-03-23 10:35:21 +0200

    Update wsgi.py

--- djangoproj/wsgi.py
+++ djangoproj/wsgi.py
@@ -4,5 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproj.settings')
 
 application = get_wsgi_application()
-# Add this line so Vercel can find the entry point
+
+# ADD THIS LINE AT THE BOTTOM
 app = application
