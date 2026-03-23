2879c61824a9ea5f64df5d6007ca4f7eb3c7de6d
Deolyn East <deolyneast@gmail.com>
2026-03-23 11:00:48 +0200

    Update app.js

--- server/frontend/src/app.js
+++ server/frontend/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Dealers from "./components/Dealers/Dealers";
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
@@ -7,12 +8,16 @@ import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dealers />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dealer/:id" element={<Dealer />} />
-      <Route path="/postreview/:id" element={<PostReview />} />
-    </Routes>
+    <div>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Dealers />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dealer/:id" element={<Dealer />} />
+        <Route path="/postreview/:id" element={<PostReview />} />
+        {/* The built-in Django Admin is accessed via /admin (Backend) */}
+      </Routes>
+    </div>
   );
 }
 export default App;
